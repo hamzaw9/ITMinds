41b923c7ca6130c65afab066132755ca74d488e1
Hamza Baig <mail.hamzabaig@gmail.com>
2023-07-30 23:00:03 +0500

    Implement hamburger menu functionality for navbar

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  /*** *******     Mobile Navbar        ****** ***/
+
+  const showMenu = document.querySelector(".menu");
+  const closeMenu = document.querySelector(".close-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  showMenu.addEventListener("click", () => {
+    navLinks.classList.add("nav-mbl-active");
+    showMenu.style.display = "none";
+    closeMenu.style.display = "block";
+  });
+
+  closeMenu.addEventListener("click", () => {
+    navLinks.classList.remove("nav-mbl-active");
+    showMenu.style.display = "block";
+    closeMenu.style.display = "none";
+  });
+});
