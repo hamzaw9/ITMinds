27d7fc8dc02b4a3d7a57d05dd7ade6fb7b666440
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-01 22:49:58 +0500

    Fix linter errors

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => {
-  /*** *******     Mobile Navbar        ****** ***/
+document.addEventListener('DOMContentLoaded', () => {
+  /** * *******     Mobile Navbar        ****** ** */
 
-  const showMenu = document.querySelector(".menu");
-  const closeMenu = document.querySelector(".close-menu");
-  const navLinks = document.querySelector(".nav-links");
+  const showMenu = document.querySelector('.menu');
+  const closeMenu = document.querySelector('.close-menu');
+  const navLinks = document.querySelector('.nav-links');
 
   function showMobileMenu() {
-    navLinks.classList.add("nav-mbl-active");
-    showMenu.style.display = "none";
-    closeMenu.style.display = "block";
+    navLinks.classList.add('nav-mbl-active');
+    showMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
   }
 
   function hideMobileMenu() {
-    navLinks.classList.remove("nav-mbl-active");
-    showMenu.style.display = "block";
-    closeMenu.style.display = "none";
+    navLinks.classList.remove('nav-mbl-active');
+    showMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
   }
 
-  closeMenu.addEventListener("click", hideMobileMenu);
-  showMenu.addEventListener("click", showMobileMenu);
+  closeMenu.addEventListener('click', hideMobileMenu);
+  showMenu.addEventListener('click', showMobileMenu);
 
-  navLinks.addEventListener("click", (event) => {
-    if (event.target.tagName === "A" || event.target.tagName === "BUTTON") {
+  navLinks.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A' || event.target.tagName === 'BUTTON') {
       hideMobileMenu();
     }
   });
 
-  /*** *******     Leading instructors        ****** ***/
+  /** * *******     Leading instructors        ****** ** */
 
   const instructors = [
     {
-      img: "images/instructor1.jpg",
-      name: "Shadman Ali",
-      subject: "Passionate Web Developer",
+      img: 'images/instructor1.jpg',
+      name: 'Shadman Ali',
+      subject: 'Passionate Web Developer',
       detail:
-        "Ability to simplify complex coding concepts, making learning enjoyable.",
+        'Ability to simplify complex coding concepts, making learning enjoyable.',
     },
     {
-      img: "images/instructor2.jpg",
-      name: "Kendra James",
-      subject: "Innovative iOS Developer",
+      img: 'images/instructor2.jpg',
+      name: 'Kendra James',
+      subject: 'Innovative iOS Developer',
       detail:
-        "Strong background in app design, encourages students to build intuitive applications.",
+        'Strong background in app design, encourages students to build intuitive applications.',
     },
     {
-      img: "images/instructor3.jpg",
-      name: "Evan Shaq",
-      subject: "Skilled Android Developer",
-      detail: "Fosters collaborative and inclusive learning environment.",
+      img: 'images/instructor3.jpg',
+      name: 'Evan Shaq',
+      subject: 'Skilled Android Developer',
+      detail: 'Fosters collaborative and inclusive learning environment.',
     },
     {
-      img: "images/instructor4.jpg",
-      name: "Chirag Agarwal",
-      subject: "Data wizard in Python",
-      detail: "Mentors aspiring data scientists to drive impactful solutions.",
+      img: 'images/instructor4.jpg',
+      name: 'Chirag Agarwal',
+      subject: 'Data wizard in Python',
+      detail: 'Mentors aspiring data scientists to drive impactful solutions.',
     },
     {
-      img: "images/instructor5.jpg",
-      name: "Divya Pritam",
-      subject: "Creative Graphic Designer",
+      img: 'images/instructor5.jpg',
+      name: 'Divya Pritam',
+      subject: 'Creative Graphic Designer',
       detail:
-        "Inspires students to explore artistic boundaries, using design as a powerful tool.",
+        'Inspires students to explore artistic boundaries, using design as a powerful tool.',
     },
     {
-      img: "images/instructor6.jpg",
-      name: "Noah Williams",
-      subject: "Leading AI Expert",
+      img: 'images/instructor6.jpg',
+      name: 'Noah Williams',
+      subject: 'Leading AI Expert',
       detail:
-        "Dedicated AI instructor employs hands-on, innovative methods to guide students.",
+        'Dedicated AI instructor employs hands-on, innovative methods to guide students.',
     },
   ];
 
   function displayInstructors(instructors) {
-    const instructorContainer = document.querySelector(".instructor-container");
+    const instructorContainer = document.querySelector('.instructor-container');
     instructors.forEach((instructor) => {
       instructorContainer.innerHTML += ` <div class="instructor">
           <div class="img-container">
